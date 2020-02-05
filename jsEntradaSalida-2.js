e4fb23609a906ce7b55c33e6c7cdb1b5fb2721f4
rainvare <49820865+rainvare@users.noreply.github.com>
2020-02-05 11:57:28 -0300

    Add files via upload
    ejercicios clase 1 de ingreso (05/02/20)

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,12 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+    // En la variable nombre guardo el ingreso del nombre
+    var nombre;
+    // Le pido el nombre
+    nombre = prompt ("Ingrese su nombre");
+    // muestro el nombre
+    alert (nombre);
+}
+
