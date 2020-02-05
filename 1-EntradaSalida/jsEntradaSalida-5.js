5437e9d402c55e926623f74450c3ce689aaa2e93
rainvare <49820865+rainvare@users.noreply.github.com>
2020-02-05 12:23:22 -0300

    Add files via upload
    u

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,6 +2,11 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
-	
+    var nombre;
+    var edad;
+    //se solicitan los datos de nombre y edad en caja de texto
+nombre = document.getElementById("elNombre").value
+edad= document.getElementById("laEdad").value
+// los datos ingresados se muestran concatenados en ventana emergente
+    alert ("Usted se llama " + nombre+ " y tiene "+ edad + " años.")
 }
-
