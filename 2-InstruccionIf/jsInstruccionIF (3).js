68cc1b236945be736a4dc48606bb555e81613266
rainvare <49820865+rainvare@users.noreply.github.com>
2020-02-06 12:15:18 -0300

    Add files via upload
    mod

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -2,5 +2,18 @@ function mostrar()
 {
 //tomo la edad  
 
+var edad;
+
+edad= document.getElementById("edad").value;
+edad= parseInt(edad);
+
+// hago la condición 1
+if (edad >= 18 )
+{ alert ("mayor de edad");
+}
+
+if (edad < 18 )
+{ alert ("menor de edad");
+}
 
 }//FIN DE LA FUNCIÓN
