8335db7ae1f28251d58a8b7d530bd65f281422b3
rainvare <49820865+rainvare@users.noreply.github.com>
2020-02-13 12:27:52 -0300

    Add files via upload
    clase 4

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -1,27 +1,8 @@
 function mostrar()
 {
-//tomo los valores de la variable 
-var mes = document.getElementById('mes').value;
+//tomo la edad  
+var mesDelAño = document.getElementById('mes').value;
 
- // uso switch para los resultados en mutliples casos
-
- switch (mes)
-{
-    case "Enero": 
-        alert ("que comiences bien el año!!!." );
-        break ;
-case  "Marzo": 
-alert ("a clases!!!.");
-break;
-
-case "Julio": 
-alert ("se vienen las vacaciones!!!.");
-break ;
-
-case "Diciembre": 
-alert ("Felices fiestas!!!.");
-break;
-}
 
 
 
