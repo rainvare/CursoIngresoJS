d6ed155e05cc15b0efc93cb878aac2cf2cd111be
rainvare <49820865+rainvare@users.noreply.github.com>
2020-02-27 09:10:22 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (7).js
+++ 6-InstruccionFor/jsIteracionesFor (7).js
@@ -1,6 +1,20 @@
 function mostrar()
 {
+    var numero = parseInt(prompt(" ingrese un numero"));
 
+    var contadorDivisores = 0;
+    
+    while (isNaN(numero )){
+        numero = parseInt(prompt("Dato inválido. reingrese número."));
+    }
+    for (var divisor= 1 ; divisor <= numero; divisor ++){
+    if (numero %  divisor ==0)
+    {
+        console.log(divisor);
+        contadorDivisores ++;
+    }
+    }
+    console.log("Cantidad de números divisores: " + contadorDivisores);
 
 
 
