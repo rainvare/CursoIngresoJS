1073214b1adcab717606e600a4582fbfac36da18
rainvare <49820865+rainvare@users.noreply.github.com>
2020-02-27 12:18:46 -0300

    Add files via upload
    ultima clase

--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -1,6 +1,23 @@
 function mostrar()
 {
 
+    var numero = parseInt(prompt(" ingrese un numero"));
+    
+    while (isNaN(numero)){
+        numero = parseInt(prompt("Dato inválido. reingrese número."));
+    }
+
+    for (numero; numero <= 1; numero++){
+        if (numero %  numero == 0)
+    {
+        console.log("el número " + numero + " es primo");
+    }
+    else {
+        console.log ("el numero " + numero + " no es primo");
+    }
+    }
+    
+
 
 
 
